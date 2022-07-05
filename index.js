066abf3a52dbba45077ca6cde61847a7ebea092b
Jessica Ho <jessicadinhho@gmail.com>
2022-07-04 22:49:35 -0700

    commit tree 750c4b1e8fe0bc8f82027655278243d5187add5d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+/*
+function superbowlWin(arr) {
+    let found = arr.find(arr => arr.result === "W");
+    if(found.result === "W"){
+          return found.year
+    }else {
+        found = undefined
+        return found
+    }
+  }
+ */ 
+/*
+  function superbowlWin(arr) {
+      let didWin
+      let found = arr.find(arr => arr.result === "W");
+      if (found.result === "W"){
+          didwin = found.year
+      }
+      return didWin
+  }
+  */
+  function superbowlWin(arr) {
+    let found 
+    if(arr.find(arr => arr.result === "W")){
+          found = arr.find(arr => arr.result === "W")
+        return found.year
+    }else {
+      return found = undefined
+    }
+  }
